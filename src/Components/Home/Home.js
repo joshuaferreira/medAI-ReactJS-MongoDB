4674652fdb89fbaf59a10d1c854ce1a3909d9af3
joshuaferreira <thejoshuaferreira@gmail.com>
2024-10-28 23:35:32 +0530

    First Commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,14 @@
 // src/Components/Home/Home.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
     <div className="container text-center mt-5">
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the app.</p>
+      <Link to="/login">Login</Link>
+      <Link to="/register">SignUp / Register</Link>
     </div>
   );
 };
